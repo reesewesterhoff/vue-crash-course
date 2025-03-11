cddf0bce09d3aea6dbcffbe281928bb4fde38004
Reese <reese.westerhoff@gmail.com>
2025-03-11 11:47:19 +0100

    make addJob inputs reactive

--- src/views/AddJobView.vue
+++ src/views/AddJobView.vue
@@ -1,10 +1,37 @@
+<script setup>
+import { reactive } from "vue";
+
+const form = reactive({
+  type: "Full-Time",
+  title: "",
+  description: "",
+  salary: "",
+  location: "",
+  company: {
+    name: "",
+    description: "",
+    contactEmail: "",
+    contactPhone: "",
+  },
+});
+
+const handleSubmit = async () => {
+  console.log("form.oifjeaw oif", form.title);
+  // try {
+
+  // } catch (error) {
+
+  // }
+};
+</script>
+
 <template>
   <section class="bg-green-50">
     <div class="container m-auto max-w-2xl py-24">
       <div
         class="bg-white px-6 py-8 mb-4 shadow-md rounded-md border m-4 md:m-0"
       >
-        <form>
+        <form @submit.prevent="handleSubmit">
           <h2 class="text-3xl text-center font-semibold mb-6">Add Job</h2>
 
           <div class="mb-4">
@@ -12,6 +39,7 @@
               >Job Type</label
             >
             <select
+              v-model="form.type"
               id="type"
               name="type"
               class="border rounded w-full py-2 px-3"
@@ -29,6 +57,7 @@
               >Job Listing Name</label
             >
             <input
+              v-model="form.title"
               type="text"
               id="name"
               name="name"
@@ -42,6 +71,7 @@
               >Description</label
             >
             <textarea
+              v-model="form.description"
               id="description"
               name="description"
               class="border rounded w-full py-2 px-3"
@@ -55,6 +85,7 @@
               >Salary</label
             >
             <select
+              v-model="form.salary"
               id="salary"
               name="salary"
               class="border rounded w-full py-2 px-3"
@@ -77,6 +108,7 @@
           <div class="mb-4">
             <label class="block text-gray-700 font-bold mb-2"> Location </label>
             <input
+              v-model="form.location"
               type="text"
               id="location"
               name="location"
@@ -93,6 +125,7 @@
               >Company Name</label
             >
             <input
+              v-model="form.company.name"
               type="text"
               id="company"
               name="company"
@@ -108,6 +141,7 @@
               >Company Description</label
             >
             <textarea
+              v-model="form.company.description"
               id="company_description"
               name="company_description"
               class="border rounded w-full py-2 px-3"
@@ -123,6 +157,7 @@
               >Contact Email</label
             >
             <input
+              v-model="form.company.contactEmail"
               type="email"
               id="contact_email"
               name="contact_email"
@@ -138,6 +173,7 @@
               >Contact Phone</label
             >
             <input
+              v-model="form.company.contactPhone"
               type="tel"
               id="contact_phone"
               name="contact_phone"
